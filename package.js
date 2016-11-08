4808d4685359bf947f0235302f5e0a9de52a83d0
Shundan Xiao <xiaoshundan@gmail.com>
2016-11-08 22:39:11 +0800

    Recommit files

---
+++ package.js
@@ -0,0 +1,23 @@
+Package.describe({
+  name: 'sherryxiao1988:nova-i18n-zh-cn',
+  version: '0.0.1',
+  // Brief, one-line summary of the package.
+  summary: "Telescope i18n package (zh-CN)",
+  // URL to the Git repository containing the source code for this package.
+  git: 'https://github.com/sherryxiao1988/nova-i18n-zh-CN',
+  // By default, Meteor will default to using README.md for documentation.
+  // To avoid submitting documentation, set this field to null.
+  documentation: 'README.md'
+});
+
+Package.onUse(function (api) {
+
+  api.versionsFrom("METEOR@1.0");
+  api.use([
+    'nova:core@0.27.3-nova'
+  ]);
+
+  api.addFiles([
+    'lib/zh_CN.js'
+  ], ["client", "server"]);
+});
